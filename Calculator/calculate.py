8eb93f6e1b9b36d80a5514440df9bcd70efca56c
dev_jaeho <33758013+jaeho0718@users.noreply.github.com>
2025-03-17 14:33:42 +0900

    Fix number input error

--- Calculator/calculate.py
+++ Calculator/calculate.py
@@ -14,13 +14,23 @@ def divide(a, b):
 if __name__ == '__main__':
     ### 사용자 입력
     print('\n첫 번째 숫자를 입력하세요.')
-    input1 = input('입력: ')
+    while True:
+        try:
+            input1 = float(input('입력: '))
+            break
+        except ValueError:
+            print("유효한 숫자를 입력하세요.")
 
     print('\n원하는 사칙연산 기호 중 하나를 선택하세요. (+, -, *, /)')
     act = input('기호: ')
 
     print('\n두번째 숫자를 입력하세요.')
-    input2 = input('입력: ')
+    while True:
+        try:
+            input2 = float(input('입력: '))
+            break
+        except ValueError:
+            print("유효한 숫자를 입력하세요.")
 
     ### 연산 수행
     if act == '+':
